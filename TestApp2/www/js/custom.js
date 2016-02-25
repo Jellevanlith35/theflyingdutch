d760a158aac8c40f59fb13546dff2ba12cdd7d9d
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-02-25 12:34:12 +0100

    Index

--- TestApp2/www/js/custom.js
+++ TestApp2/www/js/custom.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $(".location").height($( window ).height()/100*33.3);
+    //$(".location").height($( window ).height()/100*33.3);
 
-    alert($( document ).height()).
-    alert($( window ).height()).
+    $("#size").text($(window).height());
 });
