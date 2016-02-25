3153e9a7a618bab52763772ee864c2e03e37023a
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-02-25 11:00:38 +0100

    TestApp2

---
+++ TestApp2/www/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+/*$(".location.amsterdam").click(function() {
+  $(this).animate({ background-color: '#ffffff' }, 1000);
+});
+
+    $(".location.amsterdam").hover(function() {
+			   $(this).animate({ backgroundColor: "#eb1c23"}, 500);
+		}, function () {
+			   $(this).animate({ backgroundColor: "#f7a4a7"}, 500);
+		});
+*/
+
+});
