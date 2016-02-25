aec232878b0ba675b006dcaded2afd89abcf94cb
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-02-25 11:22:42 +0100

    Test

--- TestApp2/www/js/custom.js
+++ TestApp2/www/js/custom.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-  $(".location").height($( window ).height()/100*33.3);
-/*$(".location.amsterdam").click(function() {
-  $(this).animate({ background-color: '#ffffff' }, 1000);
-});
-
-    $(".location.amsterdam").hover(function() {
-			   $(this).animate({ backgroundColor: "#eb1c23"}, 500);
-		}, function () {
-			   $(this).animate({ backgroundColor: "#f7a4a7"}, 500);
-		});
-*/
-
+    $(".location").height($( window ).height()/100*33.3);
 });
