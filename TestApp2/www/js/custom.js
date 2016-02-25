2d61e09f101e38eaa5d17c82d8e803f18b02443a
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-02-25 12:37:12 +0100

    custom

--- TestApp2/www/js/custom.js
+++ TestApp2/www/js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     //$(".location").height($( window ).height()/100*33.3);
 
-    $("#size").text($(window).height());
+    $("#size").text($.mobile.getScreenHeight());
 });
